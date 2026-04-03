9ec2b34b12cc80a0424f1958a07397239f3e81e8
joelromanurbanski <joeluki2000@gmail.com>
2026-04-03 19:44:04 -0300

    segundo constructor en la clase Empleado, el cual recibirá los atributos de persona y de Empleado. falta ver como hacer que no reciba el legajo ya que se genera automáticamente

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -21,6 +21,20 @@ public class Empleado extends Persona
 
 	}
 	
+	public Empleado(String dni, String nombre, String apellido, java.time.LocalDate fechaNacimiento,
+            String genero, String direccion, String telefono, String email,
+            String puesto)
+{
+super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+
+this.puesto = puesto;
+
+
+
+
+
+}
+	
 	// Getters y setters
 	
 	public int getLegajo() 
