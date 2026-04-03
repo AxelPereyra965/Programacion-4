34274c3d80f6264851ec533812732fa80799ca98
Alejo Benitez <Abenitez051@gmail.com>
2026-04-03 18:31:30 -0300

    Actualizacion de constructores y arreglo de sintaxis LocalDate

--- src/EJERCICIO1/Persona.java
+++ src/EJERCICIO1/Persona.java
@@ -15,7 +15,29 @@ public class Persona
     private String telefono;
     private String email;
     
-    
+   //Constructor vacío 
+   public Persona() 
+   { 
+   this.nombre = "sin nombre";
+   this.fechaNacimiento = LocalDate.of(2025, 1, 1); 
+   }
+   
+   public Persona(String dni, String nombre, String apellido, LocalDate fechaNacimiento, 
+		   		String genero, String direccion, String telefono, String email) {
+	   this.dni=dni;
+	   this.nombre=nombre;
+	   this.apellido=apellido;
+	   this.fechaNacimiento=fechaNacimiento;
+	   this.genero=genero;
+	   this.direccion=direccion;
+	   this.fechaNacimiento=fechaNacimiento;
+	   this.genero=genero;
+	   this.direccion=direccion;
+	   this.telefono=telefono;
+	   this.email=email;
+	 
+   }
+   
     // Get y Set
     
 	public String getDni() 
